951b1508bf3300f6493e42140da8ee12a7dce5c5
makmakkerti <makmakkerti007@gmail.com>
2020-09-09 20:08:05 +0300

    Road movement added, reset CSS, car movement added

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const score = document.querySelector('.scores'),
 start.addEventListener('click', startGame);
 document.addEventListener('keydown', startRun);
 document.addEventListener('keyup', stopRun);
+gameArea.style.height = document.documentElement.clientHeight;
 
 car.classList.add('car');
 
@@ -19,18 +20,55 @@ const keys = {
 const settings = {
   start: false,
   score: 0,
-  speed: 3,
+  carSpeed: 3,
+  roadSpeed: 4
 }
 
 function startGame(){
   start.classList.add('hide');
+  for (let i = 0; i < 20; i += 1) {
+    const roadLine = document.createElement('div');
+    roadLine.classList.add('roadLine');
+    roadLine.style.top = `${i * 85}px`;
+    gameArea.appendChild(roadLine);
+  }
+
   settings.start = true;
   gameArea.appendChild(car);
+  settings.x = car.offsetLeft;
+  settings.y = car.offsetTop;
   requestAnimationFrame(playGame);
 }
 
+function moveRoad() {
+  let roadLines = document.querySelectorAll('.roadLine');
+  roadLines.forEach(line => {
+    line.style.top = `${line.offsetTop + settings.roadSpeed}px`;
+
+    if (line.offsetTop >= document.documentElement.clientHeight) {
+      line.style.top = '-100px';
+    }
+  });
+}
+
 function playGame(){
-  console.log('Game has started!!!');
+  if(settings.start){
+    moveRoad();
+    if(keys.ArrowLeft && settings.x > 0){
+      settings.x -= settings.carSpeed;
+    }
+    if(keys.ArrowRight && settings.x < 250){
+      settings.x += settings.carSpeed;
+    }
+    if(keys.ArrowUp && settings.y > 0){
+      settings.y -= settings.carSpeed;
+    }
+    if(keys.ArrowDown){
+      settings.y += settings.carSpeed;
+    }
+  }
+  car.style.left = `${settings.x}px`;
+  car.style.top = `${settings.y}px`;
   requestAnimationFrame(playGame);
 };
 
